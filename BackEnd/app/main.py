784aae17f5167711f441466cde7d20ad0f3acbf0
P-HamNguye-N203 <phamnguyen.work@gmail.com>
2024-04-16 20:22:42 +0700

    Code 16/4 Nguyen

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -39,8 +39,7 @@ def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @app.post("/cards/create",response_model=schemas.Card)
 def create_card(card: schemas.CardRequest, db: Session = Depends(get_db)):
-    db_package = crud.create_package(name=card.NamePackage, userid=card.UserId, db=db)
-    return crud.create_packcard(listcard=card.ListCard, packageId=db_package.id,db=db)
+    return crud.create_packcard(listcard=card.ListCard,db=db)
 
 @app.get("/cards/listcard",response_model= List[schemas.Card])
 def list_cards_package(package_id:int , db: Session = Depends(get_db)):
@@ -49,3 +48,33 @@ def list_cards_package(package_id:int , db: Session = Depends(get_db)):
 @app.get("/packages/listpackage",response_model= List[schemas.Package])
 def list_packages(user_id:int, db: Session = Depends(get_db)):
     return crud.get_packages(userId=user_id, db=db)
+
+@app.post("/packages/create")
+def create_package(package: schemas.Package, db: Session = Depends(get_db)):
+    pass
+
+@app.put("/users/update")
+def update_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    pass
+
+@app.delete("/users/detele")
+def delete_user(user: schemas.User, db: Session = Depends(get_db)):
+    pass
+
+@app.put("/cards/update")
+def update_card(card: schemas.Card, db: Session = Depends(get_db)):
+    pass
+
+@app.delete("/cards/delete")
+def delete_card(id: int,db: Session = Depends(get_db)):
+    pass
+
+@app.delete("/cards/delete_all")
+def delete_all_cards(db: Session = Depends(get_db)):
+    pass
+
+@app.delete("/package/delete")
+def delete_package(db: Session = Depends(get_db)):
+    pass
+
+
