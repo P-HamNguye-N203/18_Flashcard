1537f3b503ef3ab8a3110eb87812b516e3c4db86
Vinh <cao17012003@gmail.com>
2024-04-19 13:30:37 +0700

    chinh sua main

--- FrontEnd/Flashcard/js/main.js
+++ FrontEnd/Flashcard/js/main.js
@@ -1,19 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const subjectAreas = document.querySelector('.subject-areas');
-  const subjectList = document.querySelector('.subject-list');
   const loginButton = document.getElementById('loginButton');
   const signupButton = document.getElementById('signupButton');
   const signupNowButton = document.getElementById('signupNowButton');
-
-  // Xử lý sự kiện khi nhấp vào "Subject areas"
-  subjectAreas.addEventListener('click', function() {
-    // Toggle hiển thị/ẩn danh sách môn học
-    if (subjectList.style.display === 'none') {
-      subjectList.style.display = 'block';
-    } else {
-      subjectList.style.display = 'none';
-    }
-  });
 
   // Xử lý sự kiện khi click vào nút Log in
   if (loginButton) {
@@ -35,13 +23,4 @@ document.addEventListener('DOMContentLoaded', function() {
       window.location.href = 'signup.html'; // Chuyển hướng đến trang signup.html
     });
   }
-
-  // Xử lý sự kiện click bên ngoài để ẩn danh sách môn học khi không được click vào "Subject areas"
-  document.addEventListener('click', function(event) {
-    const targetElement = event.target;
-    const isSubjectAreasClicked = subjectAreas.contains(targetElement);
-    if (!isSubjectAreasClicked && subjectList && subjectList.style.display === 'block') {
-      subjectList.style.display = 'none';
-    }
-  });
 });
